a4d83465503570758c9fff602900c219ed37ff46
AsciiMoth <ascii@moth.contact>
2023-10-10 15:18:41 +0400

    Init nix env

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Dev env flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+      rustVersion = pkgs.rust-bin.stable.latest.default;
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            alejandra.enable = true;
+            cargo-check.enable = true;
+            clippy.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+      };
+    in {
+      devShell = pkgs.mkShell {
+        inherit (checks.pre-commit-check) shellHook;
+        buildInputs = [
+          #pkgs.just
+          (rustVersion.override {extensions = ["rust-src"];})
+        ];
+      };
+    });
+}
